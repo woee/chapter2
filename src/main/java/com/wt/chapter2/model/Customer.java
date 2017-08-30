f63a4720f78e2b49e758d9d9b53aceef016de3b8
woee <1870290126@163.com>
2017-08-30 11:55:19 +0800

    框架

--- src/main/java/com/wt/chapter2/model/Customer.java
+++ src/main/java/com/wt/chapter2/model/Customer.java
@@ -1,4 +1,104 @@
 package com.wt.chapter2.model;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+    private long id;
+    private String name;
+    private String contact;
+    private String telphone;
+    private String email;
+    private String remark;
+
+    @Id
+    @Column(name = "ID")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "CONTACT")
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    @Basic
+    @Column(name = "TELPHONE")
+    public String getTelphone() {
+        return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+        this.telphone = telphone;
+    }
+
+    @Basic
+    @Column(name = "EMAIL")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "REMARK")
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        if (id != customer.id) return false;
+        if (name != null ? !name.equals(customer.name) : customer.name != null) return false;
+        if (contact != null ? !contact.equals(customer.contact) : customer.contact != null) return false;
+        if (telphone != null ? !telphone.equals(customer.telphone) : customer.telphone != null) return false;
+        if (email != null ? !email.equals(customer.email) : customer.email != null) return false;
+        if (remark != null ? !remark.equals(customer.remark) : customer.remark != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (contact != null ? contact.hashCode() : 0);
+        result = 31 * result + (telphone != null ? telphone.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (remark != null ? remark.hashCode() : 0);
+        return result;
+    }
 }
